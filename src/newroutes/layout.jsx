2d84d98e887be620bf8871e535cc51a3d3e108ae
Aayush stha <sudipk339@gmail.com>
2025-04-30 09:35:14 +0545

    new

---
+++ src/newroutes/layout.jsx
@@ -0,0 +1,28 @@
+export default App;
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./newroutes/About";
+import Contactus from "./newroutes/Contactus";
+import Home from "./newroutes/Home";
+import Layout from "./newroutes/layout";
+
+          
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    {/* Child Routes */}
+                    <Route index element={<Home />} />
+                    <Route path="about" element={<About />} />
+                    <Route path="contactus" element={<Contactus />} />
+                    <Route path="login" element={<login />} />
+                   
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+
